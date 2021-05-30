725ed918b602c4fef52bc52863be4d8d63014dae
jaewook5267 <jawook1212@naver.com>
2021-05-31 00:38:45 +0900

    assignment_3

--- woogiProject/blog/views.py
+++ woogiProject/blog/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect
 from .models import Blog
 from django.utils import timezone
+from django.core.paginator import Paginator
+
 # Create your views here.
 
 def home(request):
-    blogs = Blog.objects
-    return render(request, 'home.html', {'blogs' : blogs})
+    blogs=Blog.objects
+    blog_list=Blog.objects.all()
+    paginator=Paginator(blog_list,2)
+    page=request.GET.get('page')
+    posts=paginator.get_page(page)
+    return render(request,'home.html',{'blogs':blogs,'posts':posts})
 
 def detail(request, blog_id):
     blog_detail = Blog.objects.get(id = blog_id)
